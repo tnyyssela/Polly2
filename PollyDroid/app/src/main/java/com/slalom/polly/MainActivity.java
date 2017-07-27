18d92eeea9d8e8a15e8084a6e035a5d765fae9d9
Josh Ruoff <josh.ruoff@slalom.com>
2017-07-26 18:31:07 -0700

    Initial commit

---
+++ PollyDroid/app/src/main/java/com/slalom/polly/MainActivity.java
@@ -0,0 +1,14 @@
+package com.slalom.polly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import dji.sdk.sdkmanager.DJISDKManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
